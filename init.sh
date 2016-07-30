7f1ac588f5d009bf8f380bb41196f966cfc3dfae
averagehuman <cyclebelfast@gmail.com>
2016-07-30 06:22:57 +0100

    more sshd config

--- init.sh
+++ init.sh
@@ -10,6 +10,12 @@ hostname -F /etc/hostname
 ip=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
 grep $LINODE_HOSTNAME /etc/hosts || echo "$ip   $ip $LINODE_HOSTNAME" >> /etc/hosts
 
+######################
+# add privileged group
+######################
+groupadd saltadmin
+usermod -a -G saltadmin $LINODE_SSH_USER
+
 #################################
 # update sshd config and restart
 #################################
@@ -19,17 +25,14 @@ tmpfile=sshd_config.tmp
 cp $origfile $tmpfile
 
 sed "s/^Port[[:space:]]\+[[:digit:]]\+$/Port $ANSIBLE_SSH_PORT/" -i $tmpfile
-sed "s/^PermitRootLogin[[:space:]].*$/PermitRootLogin no/" -i $tmpfile
+sed "s/^[#]\?PermitRootLogin[[:space:]].*$/PermitRootLogin no/" -i $tmpfile
 sed "s/^[#]\?PasswordAuthentication .*/PasswordAuthentication no/g" -i $tmpfile
+sed "s/^[#]\?ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/g" -i $tmpfile
+echo "AllowGroups saltadmin" >> $tmpfile
 echo "AddressFamily inet" >> $tmpfile
 
 mv $tmpfile $origfile
 
 systemctl restart sshd
 
-######################################################
-# create ansible log directory with correct ownership
-######################################################
-mkdir -p /var/log/ansible
-chown -R $LINODE_SSH_USER:$LINODE_SSH_USER /var/log/ansible
 
